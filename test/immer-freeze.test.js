ba9d9002ef9be8a9b64d38d08ab4bd9f11a8ef1f
wighawag <wighawag@gmail.com>
2023-11-26 07:43:41 +0000

    init

---
+++ test/immer-freeze.test.js
@@ -0,0 +1,24 @@
+import {it, describe} from 'vitest'
+import {createDraft, finishDraft} from 'immer';
+
+
+describe('immer freeze', function () {
+    it('should not freeze original object', function() {
+        const obj = {
+            list: []
+        }
+        const draft = createDraft(obj);
+        draft.list.push("one")
+        const final = finishDraft(draft);
+        obj.list.push("two");
+    })
+
+    it('should not freeze original object even if no changes were made to the draft', function() {
+        const obj = {
+            list: []
+        }
+        const draft = createDraft(obj);
+        const final = finishDraft(draft);
+        obj.list.push("one");
+    });
+})
